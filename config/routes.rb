70580bfb29a5c614d4844486efc55dd2de14d762
Colt <coltsteele1@gmail.com>
2013-12-10 14:29:37 -0800

    add header to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Hogwarts::Application.routes.draw do
-  root :to=> 'houses#index'
+  root :to => 'houses#index'
   # route to: 'welcome/index'
 
-    get '/houses' => 'houses#index'
+    # get '/houses' => 'houses#index'
     get '/students' => 'students#index'
 #   get '/students' => 'students#create'
 
