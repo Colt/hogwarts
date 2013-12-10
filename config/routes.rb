b4175cc88229f63455b42efa65cb2ecf6323b5a0
Colt <coltsteele1@gmail.com>
2013-12-10 14:15:19 -0800

    rename houses controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Hogwarts::Application.routes.draw do
-  root :to => 'houses#index'
+  root :to=> 'houses#index'
+  # route to: 'welcome/index'
 
-#   get '/houses' => 'houses#List'
-#   get '/students' => 'students#index'
+    get '/houses' => 'houses#index'
+    get '/students' => 'students#index'
 #   get '/students' => 'students#create'
 
 end
